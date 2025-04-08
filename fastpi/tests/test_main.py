b8bf483ac7d9d75a0ab2fbd8cb1b361b79afd1ec
Giiid <gideonfummey1@gmail.com>
2025-04-08 17:09:03 +0100

    Add deployment workflow

--- fastpi/tests/test_main.py
+++ fastpi/tests/test_main.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from ..db import models
-
 # Import the FastAPI app and dependencies
 from ..main import app
 from ..utils import get_db
